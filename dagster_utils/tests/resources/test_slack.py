1e42c127e65ce1e6b28623263e4f85c89598c243
Steve Pletcher <spletche@broadinstitute.org>
2021-05-04 11:49:11 -0400

    fix slack test, try perms fix for tag cut workflow

--- dagster_utils/tests/resources/test_slack.py
+++ dagster_utils/tests/resources/test_slack.py
@@ -1,16 +1,11 @@
-import os
 import unittest
-from unittest import mock
 
 from dagster_utils.resources.slack import live_slack_client, LiveSlackClient
 from dagster_utils.testing.resources import initialize_resource
 
 
 class LiveSlackResourceTestCase(unittest.TestCase):
-    @mock.patch.dict(os.environ, {
-        **os.environ,
-        'SLACK_TOKEN': 'jeepers',
-    })
     def test_resource_can_be_initialized(self):
-        with initialize_resource(live_slack_client, config={'channel': '#something'}) as client_instance:
+        test_config = {'channel': '#something', 'token': 'aeiou'}
+        with initialize_resource(live_slack_client, config=test_config) as client_instance:
             self.assertIsInstance(client_instance, LiveSlackClient)
