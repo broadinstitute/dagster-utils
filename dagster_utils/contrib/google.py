c601beee27fe60191d18c6b4546605e1784f0a78
Drew Herbst <aherbst@broadinstitute.org>
2021-07-23 14:22:59 -0400

    Add test

--- dagster_utils/contrib/google.py
+++ dagster_utils/contrib/google.py
@@ -57,5 +57,6 @@ class GsBucketWithPrefix:
 
 
 def parse_gs_path(raw_gs_path: str) -> GsBucketWithPrefix:
+    assert raw_gs_path.startswith("gs://")
     url_result = urlparse(raw_gs_path)
     return GsBucketWithPrefix(url_result.netloc, url_result.path[1:])
