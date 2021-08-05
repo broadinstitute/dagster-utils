81c59d2ca3da349515c2be4d2e121cebde6b5791
Drew Herbst <aherbst@broadinstitute.org>
2021-08-05 14:57:04 -0400

    Add more types

--- dagster_utils/contrib/data_repo/snapshot.py
+++ dagster_utils/contrib/data_repo/snapshot.py
@@ -1,22 +1,33 @@
+"""
+TDR snapshot operations
+"""
 import logging
+from enum import Enum
 
 from data_repo_client import RepositoryApi, SnapshotRequestModel, SnapshotRequestContentsModel
 
-from dagster_utils.contrib.data_repo.typing import JobId
+from dagster_utils.contrib.data_repo.typing import JobId, ProfileId, DatasetName
+
+
+class SnapshotMode(Enum):
+    BY_ASSET = "byAsset"
+    BY_FULL_VIEW = "byFullView"
+    BY_ROW_ID = "byRowId"
+    BY_QUERY = "byQuery"
 
 
 def submit_snapshot_request(
         snapshot_name: str,
-        profile_id: str,
-        dataset_name,
-        mode: str,
+        profile_id: ProfileId,
+        dataset_name: DatasetName,
+        mode: SnapshotMode,
         reader_list: list[str],
         data_repo_client: RepositoryApi) -> JobId:
     snapshot_request = SnapshotRequestModel(
         name=snapshot_name,
         profile_id=profile_id,
         description=f"Create snapshot {snapshot_name}",
-        contents=[SnapshotRequestContentsModel(dataset_name=dataset_name, mode=mode)],
+        contents=[SnapshotRequestContentsModel(dataset_name=dataset_name, mode=mode.value)],
         readers=reader_list
     )
 
