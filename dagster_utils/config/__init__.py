8a2ccf7a7ba8a42875442c6897a255d5726fc76c
John Kitonyo <johnkilonzi@outlook.com>
2023-09-05 15:29:23 -0400

    Tests rename of the subdirectory

--- dagster_utils/config/__init__.py
+++
@@ -1,6 +0,0 @@
-from dagster_utils.config.configurators import configurator_aimed_at, configurator_aimed_at_directory
-
-__all__ = [
-    'configurator_aimed_at',
-    'configurator_aimed_at_directory',
-]
